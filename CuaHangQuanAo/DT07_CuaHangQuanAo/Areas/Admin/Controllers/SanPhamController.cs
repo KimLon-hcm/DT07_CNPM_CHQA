da9170e42f3909668441f7a1f1aaa1a022576c3a
KimLon-hcm <kimlonghuit2005@gmail.com>
2025-10-25 21:33:33 +0700

    Them controll ti nua in san pham

---
+++ CuaHangQuanAo/DT07_CuaHangQuanAo/Areas/Admin/Controllers/SanPhamController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DT07_CuaHangQuanAo.Models;
+
+namespace DT07_CuaHangQuanAo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
