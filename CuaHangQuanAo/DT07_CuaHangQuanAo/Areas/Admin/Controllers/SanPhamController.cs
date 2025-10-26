57efc40a38886b34c17159fff45e17b17076438b
KimLon-hcm <kimlonghuit2005@gmail.com>
2025-10-26 21:44:53 +0700

    nâng cấp vừa thêm sp vừa thêm biến thể

--- CuaHangQuanAo/DT07_CuaHangQuanAo/Areas/Admin/Controllers/SanPhamController.cs
+++ CuaHangQuanAo/DT07_CuaHangQuanAo/Areas/Admin/Controllers/SanPhamController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DT07_CuaHangQuanAo.Areas.Admin.Controllers
 {
@@ -34,26 +35,77 @@ namespace DT07_CuaHangQuanAo.Areas.Admin.Controllers
         {
            
             ViewBag.IdloaiHang = new SelectList(_context.LoaiHangs.ToList(), "IdloaiHang", "TenLoaiHang");
-
+            ViewBag.IdmauSac = new SelectList(_context.MauSacs.ToList(), "IdmauSac", "TenMau");
             ViewBag.IdthuongHieu = new SelectList(_context.ThuongHieus.ToList(), "IdthuongHieu", "TenThuongHieu");
+            ViewBag.IdkichThuoc = new SelectList(_context.KichThuocs.ToList(), "IdkichThuoc", "TenKichThuoc");
 
-            return View();
+            return View(new SanPhamCreateViewModel());
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Them([Bind("TenHang,MoTa,IdloaiHang,IdthuongHieu,TrangThai")] HangHoa hangHoa)
+        public async Task<IActionResult> Them(SanPhamCreateViewModel model)
         {
-            ViewBag.IdloaiHang = new SelectList(_context.LoaiHangs.ToList(), "IdloaiHang", "TenLoaiHang", hangHoa.IdloaiHang);
-            ViewBag.IdthuongHieu = new SelectList(_context.ThuongHieus.ToList(), "IdthuongHieu", "TenThuongHieu", hangHoa.IdthuongHieu);
+            ViewBag.IdloaiHang = new SelectList(_context.LoaiHangs.ToList(), "IdloaiHang", "TenLoaiHang", model.IdloaiHang);
+            ViewBag.IdmauSac = new SelectList(_context.MauSacs.ToList(), "IdmauSac", "TenMau");
+            ViewBag.IdthuongHieu = new SelectList(_context.ThuongHieus.ToList(), "IdthuongHieu", "TenThuongHieu", model.IdthuongHieu);
+            ViewBag.IdkichThuoc = new SelectList(_context.KichThuocs.ToList(), "IdkichThuoc", "TenKichThuoc");
+
             if (ModelState.IsValid)
             {
-                _context.Add(hangHoa);
-                _context.SaveChanges();
-                return RedirectToAction("Index", "SanPham", new { area = "Admin" });
+                using (var transaction = await _context.Database.BeginTransactionAsync())
+                {
+                    try
+                    {
+                        // Bước 1: Tạo và lưu đối tượng HangHoa
+                        var newHangHoa = new HangHoa
+                        {
+                            TenHang = model.TenHang,
+                            MoTa = model.MoTa,
+                            IdloaiHang = model.IdloaiHang,
+                            IdthuongHieu = model.IdthuongHieu,
+                            TrangThai = model.TrangThai
+                        };
+                        _context.HangHoas.Add(newHangHoa);
+                        await _context.SaveChangesAsync(); // Lưu để EF gán ID cho newHangHoa
+
+                        // Bước 2: Lặp qua danh sách biến thể và lưu chúng
+                        if (model.BienThes != null && model.BienThes.Any())
+                        {
+                            foreach (var bienTheVM in model.BienThes)
+                            {
+                                var newBienThe = new HangHoaBienThe
+                                {
+                                    Idhang = newHangHoa.Idhang, // Gán ID của hàng hóa vừa tạo
+                                    IdmauSac = bienTheVM.IdmauSac,
+                                    IdkichThuoc = bienTheVM.IdkichThuoc,
+                                    Sku = bienTheVM.Sku,
+                                    Gia = bienTheVM.Gia,
+                                    GiaKhuyenMai = bienTheVM.GiaKhuyenMai,
+                                    SoLuongTon = bienTheVM.SoLuongTon
+                                };
+                                _context.HangHoaBienThes.Add(newBienThe);
+                            }
+                            await _context.SaveChangesAsync(); // Lưu tất cả biến thể vào DB
+                        }
+
+                        // Nếu tất cả thành công, commit transaction
+                        await transaction.CommitAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                    catch (Exception ex) // Bắt ngoại lệ và gán nó vào biến 'ex'
+                    {
+                        Console.WriteLine("!!!!!!!!!!!!!! DATABASE UPDATE EXCEPTION !!!!!!!!!!!!!!");
+                        Console.WriteLine(ex.ToString());
+                        Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+                        await transaction.RollbackAsync();
+                        ModelState.AddModelError("", "Đã xảy ra lỗi trong quá trình tạo sản phẩm. Vui lòng thử lại.");
+                    }
+                }
             }
-            
-            return View(hangHoa);
+
+            return View(model);
         }
     }
 }
