6e77d5c80216550f4b300bbc46aa8f24f20b2ea5
KimLon-hcm <kimlonghuit2005@gmail.com>
2025-10-26 21:44:14 +0700

    Thêm model để có thể gộp chung vừa thêm hàng vừa thêm biến thể

---
+++ CuaHangQuanAo/DT07_CuaHangQuanAo/Models/SanPhamCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace DT07_CuaHangQuanAo.Models
+{
+    public class SanPhamCreateViewModel
+    {
+        // Các thuộc tính cho bảng HangHoa
+        public string TenHang { get; set; }
+        public string? MoTa { get; set; }
+        public int IdloaiHang { get; set; }
+        public int IdthuongHieu { get; set; }
+        public bool TrangThai { get; set; } 
+
+        // Danh sách các biến thể
+        public List<HangHoaBienTheViewModel> BienThes { get; set; }
+
+        // Constructor để khởi tạo list, tránh lỗi null
+        public SanPhamCreateViewModel()
+        {
+            BienThes = new List<HangHoaBienTheViewModel>();
+        }
+    }
+}
